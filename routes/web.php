0aa1ba26e6679b2800453a4ea246ec4412be4a71
Jonayear <Jonayearimran@gmail.com>
2023-12-13 21:45:33 +0600

    create admin controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +30,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+// Admin Controller
+Route::controller(AdminController::class)->group(function(){
+    Route::get('/dashboard','dashboard')->name('dashboard');
+});
